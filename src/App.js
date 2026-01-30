1cb3a019b87057d12a162446c12ff82c25af7f6d
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-01-30 14:22:30 +0200

    Pohjaksi annettu sovellus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-function App() {
-  const counter = useSelector(state => state)
-  const dispatch = useDispatch()
+const App = () => {
+  const anecdotes = useSelector(state => state)
+
+  const vote = id => {
+    console.log('vote', id)
+  }
 
   return (
-   <div>
-      <div>{counter}</div>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>
-        plus
-      </button>
-      <button onClick={() => dispatch({ type: 'DECREMENT' })}>
-        minus
-      </button>
-      <button onClick={() => dispatch({ type: 'ZERO' })}>
-        zero
-      </button>
-    </div>  
-  );
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
-
-const store = createStore(counterReducer)
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
